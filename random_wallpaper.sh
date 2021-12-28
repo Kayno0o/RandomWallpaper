323feeb3300a442c27a358dbe237b80436de1d5d
kevyn <kevyn.fyleyssant@gmail.com>
2021-12-28 19:23:14 +0100

    :bug: Fix color scheme save

--- random_wallpaper.sh
+++ random_wallpaper.sh
@@ -48,12 +48,12 @@ if [ $1 != "-h" ] && [ $1 != "--help" ]; then
     echo "  Saving to $SAVE_FOLDER"
 
     echo "
-    BACKGROUND_COLOR=$(sed -n '1p' ~/.cache/wal/colors)
-    TEXT_COLOR=$(sed -n '3p' ~/.cache/wal/colors)
-    GOOD_COLOR=$(sed -n '8p' ~/.cache/wal/colors)
-    WARNING_COLOR=$(sed -n '2p' ~/.cache/wal/colors)
-    ERROR_COLOR=$(sed -n '6p' ~/.cache/wal/colors)
-    FOCUS=$(sed -n '5p' ~/.cache/wal/colors)
+BACKGROUND_COLOR=$(sed -n '1p' ~/.cache/wal/colors)
+TEXT_COLOR=$(sed -n '3p' ~/.cache/wal/colors)
+GOOD_COLOR=$(sed -n '8p' ~/.cache/wal/colors)
+WARNING_COLOR=$(sed -n '2p' ~/.cache/wal/colors)
+ERROR_COLOR=$(sed -n '6p' ~/.cache/wal/colors)
+FOCUS=$(sed -n '5p' ~/.cache/wal/colors)
     " > "$SAVE_FOLDER/color_scheme"
 
     echo "  Done"
